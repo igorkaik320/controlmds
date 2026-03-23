b02610eca4ac6d04a6ed8c3bc98e907d94330ce1
igorkaik320 <igorkaik320@gmail.com>
2026-03-23 12:28:51 -0300

    Update NavLink.tsx

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,6 +1,7 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
+iimport { NavLink as RouterNavLink, NavLinkProps, useLocation, useResolvedPath } from "react-router-dom";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
+import { confirmDraftDiscard } from "@/lib/draftGuard";
 
 interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
   className?: string;
@@ -10,10 +11,21 @@ interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
   ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+    const location = useLocation();
+    const resolvedPath = useResolvedPath(to);
+
     return (
       <RouterNavLink
         ref={ref}
         to={to}
+        onClick={(event) => {
+          props.onClick?.(event);
+          if (event.defaultPrevented) return;
+          if (location.pathname === resolvedPath.pathname) return;
+          if (!confirmDraftDiscard()) {
+            event.preventDefault();
+          }
+        }}
         className={({ isActive, isPending }) =>
           cn(className, isActive && activeClassName, isPending && pendingClassName)
         }
